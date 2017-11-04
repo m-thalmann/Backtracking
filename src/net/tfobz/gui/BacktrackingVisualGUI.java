a1961d6878bc9d85f9e5f00914974f8747374647
Matthias Thalmann <matthiasthalmann1@hotmail.de>
2017-11-04 15:47:12 +0100

    commit3

--- src/net/tfobz/gui/BacktrackingVisualGUI.java
+++ src/net/tfobz/gui/BacktrackingVisualGUI.java
@@ -1,79 +1,122 @@
 package net.tfobz.gui;
 
-import java.awt.Graphics;
-import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeModel;
 
-import net.tfobz.backtracking.*;
-
-/**
- * 
- * @author 14thamat
- *
- */
+import net.tfobz.backtracking.Knoten;
+import javax.swing.tree.DefaultTreeModel;
 
 public class BacktrackingVisualGUI extends JDialog
 {
-	JButton bb = new JButton("<<");
-	JButton b = new JButton("<");
-	JButton f = new JButton(">");
-	JButton ff = new JButton(">>");
+
+	private ArrayList<Knoten> verlauf;
+	private String[] names;
+	private int pos = 0;
+	private int size = 0;
+	private JTree tree = new JTree();
 	
-	ArrayList<Knoten> verlauf = null;
-	
-	public BacktrackingVisualGUI(JFrame owner, ArrayList<Knoten> verlauf, String[] names){
+	public BacktrackingVisualGUI(JFrame owner, ArrayList<Knoten> verlauf, String[] names) {
 		super(owner, "Backtracking - Visualisieren");
-		setLayout(null);
-		setModal(true);
-		setResizable(false);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		int width = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth() - Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration()).left - Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration()).right;
-		int height = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight() - Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration()).top - Toolkit.getDefaultToolkit().getScreenInsets(getGraphicsConfiguration()).bottom;		
-		setBounds(0, 0, width, height);
 		
 		this.verlauf = verlauf;
+		this.names = names;
+		this.size = verlauf.size();
 		
-		this.bb.setBounds((int) this.getSize().getWidth() - 4 * 60 - 20, (int) this.getSize().getHeight() - 100, 50, 40);
-		this.bb.setFocusPainted(false);
-		this.getContentPane().add(bb);
+		setResizable(false);
+		getContentPane().setLayout(null);
 		
-		this.b.setBounds((int) this.getSize().getWidth() - 3 * 60 - 20, (int) this.getSize().getHeight() - 100, 50, 40);
-		this.b.setFocusPainted(false);
-		this.getContentPane().add(b);
+		JScrollPane treeView = new JScrollPane(tree);
+		treeView.setBounds(12, 13, 458, 385);
+		getContentPane().add(treeView);
 		
-		this.f.setBounds((int) this.getSize().getWidth() - 2 * 60 - 20, (int) this.getSize().getHeight() - 100, 50, 40);
-		this.f.setFocusPainted(false);
-		this.getContentPane().add(f);
+		JButton button = new JButton("<<");
+		button.setBounds(12, 411, 50, 35);
+		button.addActionListener(new ActionListener()
+		{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pos = 0;
+				showVerlauf();
+			}
+		});
+		button.setFocusPainted(false);
+		getContentPane().add(button);
 		
-		this.ff.setBounds((int) this.getSize().getWidth() - 1 * 60 - 20, (int) this.getSize().getHeight() - 100, 50, 40);
-		this.ff.setFocusPainted(false);
-		this.getContentPane().add(ff);
+		JButton button_1 = new JButton("<");
+		button_1.setBounds(74, 411, 50, 35);
+		button_1.addActionListener(new ActionListener()
+		{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(pos > 0){
+					pos--;
+					showVerlauf();
+				}
+			}
+		});
+		button_1.setFocusPainted(false);
+		getContentPane().add(button_1);
+		
+		JButton button_2 = new JButton(">");
+		button_2.setBounds(136, 411, 50, 35);
+		button_2.addActionListener(new ActionListener()
+		{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(pos < size -1){
+					pos++;
+					showVerlauf();
+				}
+			}
+		});
+		button_2.setFocusPainted(false);
+		getContentPane().add(button_2);
+		
+		JButton button_3 = new JButton(">>");
+		button_3.setBounds(198, 411, 50, 35);
+		button_3.addActionListener(new ActionListener()
+		{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pos = size -1;
+				showVerlauf();
+			}
+		});
+		getContentPane().add(button_3);
+		
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setModal(true);
+		
+		showVerlauf();
+		
+		for(int i = 0; i < size; i++)
+			System.out.println(verlauf.get(i).toString(true));
 	}
 	
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		
-		g.drawString("Root", getX(0, 0), 100);
+	private void showVerlauf(){
+		tree.setModel(new DefaultTreeModel(
+				new DefaultMutableTreeNode("Start") {
+					{
+						for(int i = 0; i < pos; i++){
+							add(new DefaultMutableTreeNode(verlauf.get(i).getContent()));
+						}
+					}
+				}
+			));
 	}
-	
-	private int getX(int level, int num) {
-		int width = this.getContentPane().getWidth() - this.getContentPane().getInsets().left - this.getContentPane().getInsets().right - 100;
-		int ret = this.getContentPane().getInsets().left + 50;
-		
-		int n = (int) Math.pow(2, level);
-		
-		ret += (width / n) * num;
-				
-		return ret;
-	}
-	
-	private int getY(int level) {
-		return 0;
-	}
-	
 }

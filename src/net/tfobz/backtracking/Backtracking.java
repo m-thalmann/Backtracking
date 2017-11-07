8ab6340b7d5225498c865b237b08938316f0ab98
Patunter <32960863+Patunter@users.noreply.github.com>
2017-11-07 16:39:56 +0100

    commit 8
    Commentary added to Backtracking

--- src/net/tfobz/backtracking/Backtracking.java
+++ src/net/tfobz/backtracking/Backtracking.java
@@ -2,16 +2,47 @@ package net.tfobz.backtracking;
 
 import java.util.ArrayList;
 
-public class Backtracking{
 
+/**
+ * Diese Klasse enthällt Methoden und Eigenschaften,
+ * mit deren Hilfe man den Backtracking-Algorithmus anwendet um 
+ * das gegebene "Rucksackproblem zu lösen"
+ * 
+ * 
+ * @author 14untpat
+ *
+ */
+public class Backtracking{
+	
+	//Das Maxiamlgewicht des Rucksacks
 	private static int max_weigth = 0;
+	
+	//Mögliche Elemente mit ihrem Gewicht und Wert
 	private static int[][] items = null;
+	
+	//Wert vom bisher vielversprechendsten Zweig
 	private static int maxSchranke = 0;
+	
+	//Bisher bester Knoten
 	private static Knoten best = null;
+	
+	//Einige zu Knoten hinzugefügte Eigenschaften und Methoden (Zur Errechnung des Verlaufs)
 	private static KnotenExt bestExt = null;
 	
+	//Knoten, die bis besten Knoten durchlaufen wurden
 	private static ArrayList<KnotenExt> verlauf = null;
 	
+	/**
+	 *Setzt die Items und das Maximalgewicht.
+	 *Ruft die Methode computeBest auf, welche das Ergebnis in best schreibt.
+	 *Das Ergebnis wird dann über die Methode getElements von Knoten aufgerufen.
+	 * 
+	 * @param i Elemente, die zur Berechnung verwendet werden
+	 * @param maxWeight Maximalgewicht im Rucksack
+	 * 
+	 * @return Elemente, die im Besten Knoten enthalten sind
+	 * @throws BacktrackingException => Wenn entweder das Gewicht oder die Items ungültige Werte haben
+	 */
 	public static int[] getBest(int[][] i, int maxWeight) throws BacktrackingException{
 		if(i == null){
 			return null;
@@ -33,6 +64,15 @@ public class Backtracking{
 		return best.getElements();
 	}
 	
+	/**
+	 * Methode, die mit den Knoten und dem Level über Backtracking die beste Lösung errechnet.
+	 * Die Lösung wird mithilfe der Schranke und dem bisher besten Knoten errechnet:
+	 * Ist der jetzige Knoten besser als der bisher beste und auf dem Letzten Level,
+	 * wird er als Bester gestzt.
+	 * 
+	 * @param k Knoten bei dem sich die Methode befindet
+	 * @param level Level im Baum der Möglichkeiten auf dem sich das Programm befindet
+	 */
 	private static void computeBest(Knoten k, int level){
 		if(level == items.length){
 			maxSchranke = k.getValue();
@@ -63,7 +103,15 @@ public class Backtracking{
 				computeBest(r, level + 1);
 		}
 	}
-
+	
+	/**
+	 * Funktioniert wie getBest, wobei der Verlauf (alle durchlaufenen Knoten) in verlauf abgespeichert wird.
+	 * 
+	 * @param i Items
+	 * @param maxWeight Maxiamlgewicht
+	 * @return Alle durchlaufenen Knoten
+	 * @throws BacktrackingException Wenn Gewicht oder Items ungültig sind
+	 */
 	public static ArrayList<KnotenExt> getVerlauf(int[][] i, int maxWeight) throws BacktrackingException{
 		if(i == null){
 			return null;
@@ -85,7 +133,12 @@ public class Backtracking{
 		
 		return verlauf;
 	}
-	
+	 /**
+	  * Wie computeBest, nur dass der Verlauf in der ArrayList verlauf abgespeichert wird.
+	  * 
+	  * @param k Knoten bei dem man sich befindet
+	  * @param level Level auf dem man sich befindet
+	  */
 	private static void computeVerlauf(KnotenExt k, int level){
 		if(level == items.length){
 			maxSchranke = k.getValue();
